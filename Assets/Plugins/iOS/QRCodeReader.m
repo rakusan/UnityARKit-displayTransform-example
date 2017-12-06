94fac4a7cc7ca3da1e94f458e3108311741c0e77
rakusan <rakusan@sonicmoov.com>
2017-12-06 11:57:10 +0900

    initial commit

---
+++ Assets/Plugins/iOS/QRCodeReader.m
@@ -0,0 +1,52 @@
+//
+//  QRCodeReader.m
+//  Unity-iPhone
+//
+//  Created by Yoshikazu Kuramochi on 2017/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+static float qrcodeCorners[8];
+static volatile BOOL reading = false;
+
+void ReadQRCode(long long mtlTexPtr)
+{
+    if (reading) return;
+    reading = YES;
+    
+    MTLTextureRef mtlTex = (__bridge MTLTextureRef) (void*) mtlTexPtr;
+    CIImage *ciImage = [CIImage imageWithMTLTexture:mtlTex options:nil];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        CGFloat iw = ciImage.extent.size.width;
+        CGFloat ih = ciImage.extent.size.height;
+        
+        CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode context:nil options:nil];
+        NSArray<CIFeature *> *features = [detector featuresInImage:ciImage];
+        
+        if (features.count > 0) {
+            CIQRCodeFeature *feature = (CIQRCodeFeature*) [features objectAtIndex:0];
+            
+            qrcodeCorners[0] = feature.topLeft.x / iw;
+            qrcodeCorners[1] = feature.topLeft.y / ih;
+            qrcodeCorners[2] = feature.topRight.x / iw;
+            qrcodeCorners[3] = feature.topRight.y / ih;
+            qrcodeCorners[4] = feature.bottomLeft.x / iw;
+            qrcodeCorners[5] = feature.bottomLeft.y / ih;
+            qrcodeCorners[6] = feature.bottomRight.x / iw;
+            qrcodeCorners[7] = feature.bottomRight.y / ih;
+            
+            UnitySendMessage("QRCodeReader", "OnReadQRCode", "");
+        }
+        
+        reading = NO;
+    });
+}
+
+void GetQRCodeCorners(int32_t **cornersPtr)
+{
+    float *floatArray = malloc(sizeof(float) * 8);
+    memcpy(floatArray, qrcodeCorners, sizeof(qrcodeCorners));
+    *cornersPtr = (int32_t*) floatArray;
+}
---
+++ Assets/Scripts/QRCodeReader.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class QRCodeReader : MonoBehaviour {
+
+#if UNITY_IPHONE && !UNITY_EDITOR
+
+	[DllImport ("__Internal")]
+	private static extern void ReadQRCode(long mtlTexPtr);
+
+	[DllImport ("__Internal")]
+	private static extern void GetQRCodeCorners(out IntPtr cornersPtr);
+
+	private static float[] GetQRCodeCorners() {
+		IntPtr cornersPtr;
+		GetQRCodeCorners (out cornersPtr);
+		float[] corners = new float[8];
+		Marshal.Copy (cornersPtr, corners, 0, 8);
+		return corners;
+	}
+
+#else
+
+	private static void ReadQRCode(long mtlTexPtr) {
+	}
+
+	private static float[] GetQRCodeCorners() {
+		return new float[8];
+	}
+
+#endif
+
+	[SerializeField]
+	private Material material;
+
+	private UnityARSessionNativeInterface arSession;
+	private Matrix4x4 displayTransformInverse;
+	private Vector3[] corners = new Vector3[4];
+
+
+	void Start () {
+		arSession = UnityARSessionNativeInterface.GetARSessionNativeInterface ();
+		UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
+	}
+	
+	private void ARFrameUpdated(UnityARCamera camera) {
+		Matrix4x4 tmp = new Matrix4x4 (
+			camera.displayTransform.column0,
+			camera.displayTransform.column1,
+			camera.displayTransform.column2,
+			camera.displayTransform.column3
+		);
+		displayTransformInverse = tmp.inverse;
+	}
+
+	void Update () {
+		ARTextureHandles handles = arSession.GetARVideoTextureHandles ();
+		if (handles.textureY != System.IntPtr.Zero) {
+			ReadQRCode (handles.textureY.ToInt64 ());
+		}
+	}
+
+	private Vector3 VideoTextureToViewportPoint(Vector2 videoTexturePoint) {
+		Vector4 column0 = displayTransformInverse.GetColumn(0);
+		Vector4 column1 = displayTransformInverse.GetColumn(1);
+		float x = column0.x * videoTexturePoint.x + column0.y * videoTexturePoint.y + column0.z;
+		float y = column1.x * videoTexturePoint.x + column1.y * videoTexturePoint.y + column1.z;
+		return new Vector3 (x, y);
+	}
+
+	void OnReadQRCode(string arg) {
+		float[] videoTexCorners = GetQRCodeCorners ();
+
+		corners[0] = VideoTextureToViewportPoint(new Vector2 (videoTexCorners [0], videoTexCorners [1]));
+		corners[1] = VideoTextureToViewportPoint(new Vector2 (videoTexCorners [2], videoTexCorners [3]));
+		corners[2] = VideoTextureToViewportPoint(new Vector2 (videoTexCorners [4], videoTexCorners [5]));
+		corners[3] = VideoTextureToViewportPoint(new Vector2 (videoTexCorners [6], videoTexCorners [7]));
+	}
+
+	void OnRenderObject() {
+		material.SetPass (0);
+
+		GL.PushMatrix ();
+		GL.LoadOrtho ();
+
+		GL.Begin (GL.QUADS);
+
+		GL.Vertex (corners[0]);
+		GL.Vertex (corners[1]);
+		GL.Vertex (corners[3]);
+		GL.Vertex (corners[2]);
+
+		GL.End ();
+		GL.PopMatrix ();
+	}
+}
